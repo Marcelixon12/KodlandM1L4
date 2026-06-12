bc32d03ca321e34e0ad8c3c345dd68b35ef9f502
Marcelixon12 <marcelidybiec@gmail.com>
2026-06-12 18:32:11 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import random
+import asyncio
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+
+@bot.command()
+async def hello(ctx): # $hello
+    await ctx.send(f'Cześć, jestem bot{bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+bot.run("")
