530cb06a57e45d73d58e97c023e49872366b33f2
Marcelixon12 <marcelidybiec@gmail.com>
2026-06-12 18:37:19 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,12 +13,27 @@ async def on_ready():
     print(f'Zalogowaliśmy się jako {bot.user}')
 
 @bot.command()
-async def hello(ctx): # $hello
+async def hello(ctx):
     await ctx.send(f'Cześć, jestem bot{bot.user}!')
 
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+@bot.command()
+async def guess(ctx):
+    await ctx.send("Zgadnij liczbę od 1 do 10")
 
+    def is_correct(m):
+        return m.author == ctx.author and m.content.isdigit() 
+    answer = random.randint(1, 10)
+
+    try:
+        guess = await bot.wait_for('message', check=is_correct, timeout=5.0)
+    except asyncio.TimeoutError:
+        return await ctx.send(f'Odpowiadasz za długo, prawidłowa odpowiedź to {answer}.')
+    if int(guess.content) == answer:
+        await ctx.send('Masz rację. Brawo!')
+    else:
+        await ctx.send(f'Źle! Prawidłowa odpowiedź to {answer}.')
 
 bot.run("")
